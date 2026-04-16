7d0973ee81f0b9a0e5b5491c2fa5bea0979348e9
DushaniNaveendhya <dushaninaveendhya@gmail.com>
2026-04-16 11:34:56 +0530

    Update databasse queries

--- appointment-service/src/config/appointmentdb.js
+++ appointment-service/src/config/appointmentdb.js
@@ -3,6 +3,9 @@ const config = require('./appointmentdatabase');
 
 const pool = new Pool({
   connectionString: config.databaseUrl,
+  ssl: process.env.NODE_ENV === 'production' 
+    ? { rejectUnauthorized: false } 
+    : false // Disable SSL in local development to avoid connection errors
 });
 
 module.exports = pool;
